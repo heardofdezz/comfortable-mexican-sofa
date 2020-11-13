4bbb18fb5c06add9796ef19013571c5ebd97b372
Max <louamax@gmail.com>
2020-11-13 14:03:29 +0100

    pull r

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ Bundler.require(*Rails.groups)
 
 module ComfortableMexicanSofa
   class Application < Rails::Application
+    # Ensuring that ActiveStorage routes are loaded before Comfy's globbing
+    # route. Without this file serving routes are inaccessible.
+    config.railties_order = [ActiveStorage::Engine, :main_app, :all]
 
     require_relative "../lib/comfortable_mexican_sofa"
 
